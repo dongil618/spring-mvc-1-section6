bd29c72dd09037b6f1baed62bd2d0e98ed5b5f00
dongil618 <rlaehddlf618@gmail.com>
2023-08-10 18:11:08 +0900

    chore : initial setting

---
+++ src/main/java/hello/springmvc/SpringMvc1Section6Application.java
@@ -0,0 +1,13 @@
+package hello.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc1Section6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc1Section6Application.class, args);
+    }
+
+}
